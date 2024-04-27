f6b8b9b1a0ae80fd825c46fbfb45215fc784797d
sham-414 <gvstvs@gmail.com>
2024-04-28 04:28:22 +0700

    Update body

---
+++ resources/views/layout/app/header.blade.php
@@ -0,0 +1,8 @@
+<nav class="navbar navbar-light bg-light">
+        <div class="container-fluid">
+        <a class="navbar-brand" href="#">
+    <img src="{{url('img/LOGO_LBI.png')}}" alt="" width="30" height="30" class="d-inline-block align-text-top">
+</a>
+  <p id="judul"></p>
+        </div>
+    </nav>
